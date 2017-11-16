e0bfea13c64b82e4adf83ff53d16b7511658cb49
unknown <jose_94_5@hotmail.com>
2017-11-16 19:29:07 +0000

    Xamarin client/Ceaser cipher

---
+++ XamarinYSlim/XamarinYSlim/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinYSlim
+{
+    public partial class MainPage : ContentPage
+    {
+        RestClient cliente;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            button.Clicked += Button_Clicked;
+            cliente = new RestClient();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            texto.Text = await cliente.Get(palabra.Text.ToLower());
+            texto.IsVisible = true;
+        }
+    }
+}
---
+++ XamarinYSlim/XamarinYSlim/RestClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinYSlim
+{
+    class RestClient
+    {   
+        public async Task<string> Get(string palabra) {
+            try{
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync("http://bgeundam.000webhostapp.com/testSlim/index.php/cifrar/"+palabra);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK) {
+                    var codificado = await response.Content.ReadAsStringAsync();
+                    return codificado;
+                }
+            }
+            catch {
+
+            }
+            return "Fallo";
+        }
+    }
+}
